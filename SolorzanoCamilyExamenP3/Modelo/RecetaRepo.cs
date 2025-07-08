673f11c0eec0a1441d9e7b0c6984534e4a6e20ef
Camilyyyy <camilystefaniasoza@gmail.com>
2025-07-08 08:48:27 -0500

    Creacion RecetaRepo

---
+++ SolorzanoCamilyExamenP3/Modelo/RecetaRepo.cs
@@ -0,0 +1,40 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolorzanoCamilyExamenP3.Modelo
+{
+    public class RecetaRepo
+    {
+        string _dbPath;
+        public string StatusMessage { get; set; }
+        public SQLiteConnection conn { get; set; }
+
+        private void Init()
+        {
+            if (conn! == null)
+                return;
+            conn = new SQLiteConnection(_dbPath);
+            conn.CreateTable<Receta>();
+        }
+
+        public RecetaRepo(string _dbPath)
+        {
+            _dbPath = _dbPath;
+        }
+
+        public void AddNewReceta(string nombre, string ingredientes, int TiempoPreparacionMinutos, bool EsVegetariana)
+        {
+            int result = 0;
+            try
+            {
+                Init();
+                if 
+            }
+        }
+    }
+
+}
