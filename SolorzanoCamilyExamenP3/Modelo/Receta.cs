10ca8b1f818c2419c456327656b0d6df0e73b217
Camilyyyy <camilystefaniasoza@gmail.com>
2025-07-08 07:58:51 -0500

    Creacion de modelo

---
+++ SolorzanoCamilyExamenP3/Modelo/Receta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolorzanoCamilyExamenP3.Modelo
+{
+    using SQLite;
+    using System.ComponentModel;
+
+    public class Receta : INotifyPropertyChanged
+    {
+        private int idReceta;
+        private String nombreReceta { get; set; }
+        private String ingredientesReceta { get; set;}
+        private int TiempoPreparacionMinutos { get; set;}
+        private bool EsVegetariana { get; set;}
+
+        public Receta()
+        {
+            nombreReceta = string.Empty;
+            ingredientesReceta = string.Empty;
+            TiempoPreparacionMinutos = 0;
+            EsVegetariana = false;
+        }
+
+        public Receta (string nombreRecetap, string ingredientesRecetap, int TiempoPreparacionMinutosp, bool EsVegetarianap)
+        {
+
+        }
+    }
+}
