1cb4c478be3e72191427d7742f957e39cdfbde0a
Camilyyyy <camilystefaniasoza@gmail.com>
2025-07-08 08:26:45 -0500

    Clase receta terminada

--- SolorzanoCamilyExamenP3/Modelo/Receta.cs
+++ SolorzanoCamilyExamenP3/Modelo/Receta.cs
@@ -8,15 +8,16 @@ namespace SolorzanoCamilyExamenP3.Modelo
 {
     using SQLite;
     using System.ComponentModel;
+    using System.Runtime.CompilerServices;
     using Windows.Media.Capture.Frames;
 
     public class Receta : INotifyPropertyChanged
     {
         private int idReceta;
         private String nombreReceta { get; set; }
-        private String ingredientesReceta { get; set;}
-        private int TiempoPreparacionMinutos { get; set;}
-        private bool EsVegetariana { get; set;}
+        private String ingredientesReceta { get; set; }
+        private int TiempoPreparacionMinutos { get; set; }
+        private bool EsVegetariana { get; set; }
 
         public Receta()
         {
@@ -26,7 +27,7 @@ namespace SolorzanoCamilyExamenP3.Modelo
             EsVegetariana = false;
         }
 
-        public Receta (string nombreRecetap, string ingredientesRecetap, int TiempoPreparacionMinutosp, bool EsVegetarianap)
+        public Receta(string nombreRecetap, string ingredientesRecetap, int TiempoPreparacionMinutosp, bool EsVegetarianap)
         {
             nombreReceta = nombreRecetap ?? string.Empty;
             ingredientesReceta = ingredientesRecetap ?? string.Empty;
@@ -40,14 +41,48 @@ namespace SolorzanoCamilyExamenP3.Modelo
             get => idReceta;
             set => SetProperty(ref idReceta, value);
         }
+        [MaxLength(80)]
+        public string nombreReceta;
+         {
+            get => nombreReceta;
+            set => SetProperty(ref nombreReceta, value);
+        }
+
+        [MaxLength(1000)]
+        public string ingredientesReceta;
+         {
+            get => ingredientesReceta;
+            set => SetProperty(ref ingredientesReceta, value);
+        }
 
 
+        public int TiempoPreparacionMinutos;
+         {
+            get => TiempoPreparacionMinutos;
+            set => SetProperty(ref TiempoPreparacionMinutos, value);
+        }
 
+        public bool EsVegetariana;
+         {
+            get => EsVegetariana;
+            set => SetProperty(ref EsVegetariana, value);
+        }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
+        protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
 
-
-
+            backingStore = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
 
     }
 }
