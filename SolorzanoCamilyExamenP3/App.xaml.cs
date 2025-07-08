bfda2edf31acb382e69f96d348d31fc52c924b0d
laboratorio <laboratorio@UPOAULA10724>
2025-07-08 07:19:32 -0500

    Agregar archivos de proyecto.

---
+++ SolorzanoCamilyExamenP3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SolorzanoCamilyExamenP3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
